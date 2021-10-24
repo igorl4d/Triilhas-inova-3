39f9f23cfa7c9a41bb88a1e0fc8f6ae9dc29be80
Igor Lima Pereira <igorl4d@gmail.com>
2021-10-24 18:17:21 -0300

    Projeto Final

--- ong.html/script.js
+++ ong.html/script.js
@@ -1,8 +1,18 @@
 const menuHamburguer = document.querySelector('.menu-hamburguer');
 const menu = document.querySelector('.menu');
-const html = document.querySelector('html');
-console.log('DSADASDSASDA');
+const body = document.querySelector('body');
+const menuBarra = document.querySelector('.menu_barra')
+
 menuHamburguer.addEventListener('click', () => {
   menu.classList.toggle('show');
-  console.log('dsdddsds');
+  body.classList.toggle('show');
 });
+
+itemNav = document.querySelectorAll('.menu_item')
+
+for (var i = 0; i < itemNav.length; i++) {
+  itemNav[i].addEventListener('click', () => {
+    menu.classList.toggle('show');
+    body.classList.toggle('show');
+  })
+}

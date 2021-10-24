ecdae44a41ef4ccb8bbbab5aae697fbb7d0d9635
Igor Lima Pereira <igorl4d@gmail.com>
2021-10-24 11:24:18 -0300

    Melhorias CSS e JavaScript

---
+++ ong.html/script.js
@@ -0,0 +1,8 @@
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+const menu = document.querySelector('.menu');
+const html = document.querySelector('html');
+console.log('DSADASDSASDA');
+menuHamburguer.addEventListener('click', () => {
+  menu.classList.toggle('show');
+  console.log('dsdddsds');
+});
